a41b5a37122d2d4736d0bdb064844b084f3a9119
CowenZ <cowen_zuo@163.com>
2025-05-30 19:01:29 +0800

    添加渲染视图设置窗口部件类

--- src/ui/SceneViewerSettings.cpp
+++ src/ui/SceneViewerSettings.cpp
@@ -1,10 +1,30 @@
 #include "SceneViewerSettings.h"
 
-SceneViewerSettings::SceneViewerSettings(QWidget *parent)
+SceneViewerSettings::SceneViewerSettings(QWidget* parent)
 	: QWidget(parent)
 	, ui(new Ui::SceneViewerSettingsClass())
 {
 	ui->setupUi(this);
+
+	ui->hSliderMaxThreshold->setRange(0, 1000);
+	connect(ui->hSliderMaxThreshold, &QSlider::valueChanged, this, [&](int value) {
+		if (preMaxValue <= 0)
+			return;
+		auto newvalue = value / 1000.0 * preMaxValue;
+		ui->labelMaxThreshold->setText(QString::number(newvalue));
+		emit maxThresholdChanged(newvalue);
+		});
+
+	ui->hSliderRadiationThreshold->setRange(0, 1000);
+	connect(ui->hSliderRadiationThreshold, &QSlider::valueChanged, this, [&](int value) {
+		if (preRadiationValue <= 0)
+			return;
+		auto newvalue = value / 1000.0 * preRadiationValue;
+		ui->labelRadiationThreshold->setText(QString::number(newvalue));
+		emit radiationThresholdChanged(newvalue);
+		});
+
+	connect(ui->comboBox, &QComboBox::currentTextChanged, this, &SceneViewerSettings::currentWeightChanged);
 }
 
 SceneViewerSettings::~SceneViewerSettings()
@@ -15,4 +35,28 @@ SceneViewerSettings::~SceneViewerSettings()
 QString SceneViewerSettings::getCurrentWeight()
 {
 	return ui->comboBox->currentText();
+}
+
+void SceneViewerSettings::resetMaxThresholdValue(float value)
+{
+	preMaxValue = value;
+	ui->labelMaxThreshold->setText(QString::number(value));
+	ui->hSliderMaxThreshold->setValue(1000);
+}
+
+float SceneViewerSettings::getMaxThresholdValue()
+{
+	return ui->labelMaxThreshold->text().toFloat();
+}
+
+void SceneViewerSettings::resetRadiationThresholdValue(float value)
+{
+	preRadiationValue = value;
+	ui->labelRadiationThreshold->setText(QString::number(value));
+	ui->hSliderRadiationThreshold->setValue(1000);
+}
+
+float SceneViewerSettings::getRadiationThresholdValue()
+{
+	return ui->labelRadiationThreshold->text().toFloat();
 }
--- src/ui/SceneViewerSettings.h
+++ src/ui/SceneViewerSettings.h
@@ -12,10 +12,27 @@ class SceneViewerSettings : public QWidget
 	Q_OBJECT
 
 public:
-	SceneViewerSettings(QWidget *parent = nullptr);
+	SceneViewerSettings(QWidget* parent = nullptr);
 	~SceneViewerSettings();
 
 	QString getCurrentWeight();
+
+	void resetMaxThresholdValue(float max);
+
+	float getMaxThresholdValue();
+
+	void resetRadiationThresholdValue(float max);
+
+	float getRadiationThresholdValue();
+
+signals:
+	void currentWeightChanged(const QString& value);
+	void maxThresholdChanged(float value);
+	void radiationThresholdChanged(float value);
+
 private:
-	Ui::SceneViewerSettingsClass *ui;
+	Ui::SceneViewerSettingsClass* ui;
+
+	float preMaxValue{ -1.0 };
+	float preRadiationValue{ -1.0 };
 };
