585fcde0386628c76b6816be94bda7d733bcaf6f
CowenZ <cowen_zuo@163.com>
2025-05-30 19:02:32 +0800

    添加渲染更新控制接口

--- src/app/SceneCtrl.cpp
+++ src/app/SceneCtrl.cpp
@@ -1,6 +1,7 @@
 #include "SceneCtrl.h"
 
 #include <osgDB/ReadFile>
+#include <osg/ShapeDrawable>
 
 #include "3dviewer/SceneViewer.h"
 
@@ -12,12 +13,71 @@ SceneCtrl::~SceneCtrl()
 {
 }
 
-bool SceneCtrl::installSimRender(QVector<SensorPositon> sp, float max)
+bool SceneCtrl::installSimRender(QVector<SensorPositon> sp)
 {
-	return false;
+	if (!_model.valid())
+		_model = _sceneViewer->getModelNode();
+	if (!_model.valid())
+		return false;
+
+	auto pStateSet = _model->getOrCreateStateSet();
+	if (!_simRender.valid())
+	{
+		_simRender = new osg::Program;
+		_simRender->addShader(new osg::Shader(osg::Shader::VERTEX, SHADER_VERT));
+		_simRender->addShader(new osg::Shader(osg::Shader::FRAGMENT, SHADER_FRAG));
+		osg::ref_ptr<osg::Uniform> valueNumUniform = new osg::Uniform(osg::Uniform::INT, "uValueNum");//有效点的数量
+		osg::ref_ptr<osg::Uniform> posUniform = new osg::Uniform(osg::Uniform::FLOAT_VEC3, "uPositions", 20);//最大20个，不做动态的了，要和shader里面配对!
+		osg::ref_ptr<osg::Uniform> valuesUniform = new osg::Uniform(osg::Uniform::FLOAT, "uValues", 20);
+		valueNumUniform->set(0);
+		for (int i = 0;i < 20;++i)
+		{
+			posUniform->setElement(i, osg::Vec3f(0.0f, 0.0f, 0.0f));
+			valuesUniform->setElement(i, 0.0f);
+		}
+
+		pStateSet->addUniform(valueNumUniform.get());
+		pStateSet->addUniform(posUniform.get());
+		pStateSet->addUniform(valuesUniform.get());
+	}
+	pStateSet->getUniform("uValueNum")->set(sp.count());
+	auto vuniform = pStateSet->getUniform("uPositions");
+	for (int i = 0; i < sp.count(); i++)
+	{
+		vuniform->setElement(i, osg::Vec3f(float(sp[i].x), float(sp[i].y), float(sp[i].z)));
+
+		osg::ref_ptr<osg::Geode> geode = new osg::Geode();
+		geode->addDrawable(new osg::ShapeDrawable(new osg::Sphere(osg::Vec3f(float(sp[i].x), float(sp[i].y), float(sp[i].z)), 0.1f)));
+		_sceneViewer->getRootNode()->addChild(geode.get());
+	}
+	pStateSet->setAttributeAndModes(_simRender, osg::StateAttribute::ON);
+	return true;
+}
+
+bool  SceneCtrl::updateSimValues(QVector<float> values)
+{
+	if (!_simRender.valid() || !_model.valid())
+		return false;
+
+	auto pStateSet = _model->getOrCreateStateSet();
+	int valueNum = 0;
+	if (!(pStateSet->getUniform("uValueNum")->get(valueNum)))
+		return false;
+	if (valueNum != values.count())
+		return false;
+	auto vuniform = pStateSet->getUniform("uValues");
+	for (int i = 0; i < valueNum; i++)
+	{
+		vuniform->setElement(i, values[i]);
+	}
+	return true;
 }
 
 bool SceneCtrl::uninstallSimRender()
 {
-	return false;
+	if (!_simRender.valid() || !_model.valid())
+		return false;
+	auto pStateSet = _model->getOrCreateStateSet();
+	pStateSet->getUniform("uValueNum")->set(0);
+	return true;
 }
--- src/app/SceneCtrl.h
+++ src/app/SceneCtrl.h
@@ -3,24 +3,104 @@
 #include <QObject>
 
 #include <osg/Node>
+#include <osg/Program>
 
 #include "ProjectData.h"
+
+#define SHADER_VERT R"(
+	#version 110
+
+	uniform mat4 osg_ViewMatrixInverse;
+
+	varying vec3 vecWorldPos;
+	varying vec3 vecNormal;
+
+	void main()
+	{
+	  gl_TexCoord[0] = gl_MultiTexCoord0;
+	  vecWorldPos = (osg_ViewMatrixInverse * gl_ModelViewMatrix * gl_Vertex).xyz;
+	  gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+	  vecNormal = normalize( (gl_NormalMatrix * gl_Normal).xyz );
+	}
+)"
+#define SHADER_FRAG R"(
+	#version 110
+
+	uniform int uValueNum;
+	uniform vec3 uPositions[20];
+	uniform float uValues[20];
+
+	varying vec3 vecWorldPos;
+	varying vec3 vecNormal;
+
+	vec4 HSLToRGB(float H, float S, float L)
+	{	
+		H = mod(H, 360.0);
+		if (H < 0.0) H += 360.0;
+		float C = (1.0 - abs(2.0 * L - 1.0)) * S;
+		float X = C * (1.0 - abs(mod(H / 60.0, 2.0) - 1.0));
+		float m = L - C / 2.0;
+		float R, G, B;
+		if (H >= 0.0 && H < 60.0){R = C; G = X; B = 0.0;}
+		else if (H >= 60.0 && H < 120.0){R = X; G = C; B = 0.0;}
+		else if (H >= 120.0 && H < 180.0){R = 0.0; G = C; B = X;}
+		else if (H >= 180.0 && H < 240.0){R = 0.0; G = X; B = C;}
+		else if (H >= 240.0 && H < 300.0){R = X; G = 0.0; B = C;}
+		else{R = C; G = 0.0; B = X;}
+		return vec4((R + m), (G + m), (B + m), 1.0);
+	}
+	void main()
+	{
+		vec4 color = vec4(0.5,0.5,0.5,1.0);
+		if(0<uValueNum){
+			float len[20];
+			float lengthAll=0.0;
+			for(int i=0;i<uValueNum;++i)
+			{
+				vec3 pos = uPositions[i];
+				len[i] = 1.0 / pow(length(vecWorldPos - pos.xyz),2.0);
+				lengthAll += len[i];
+			}
+			float fValueAll=0.0;
+			for(int i=0;i<uValueNum;++i)
+			{
+				fValueAll += (uValues[i] *  len[i]);
+			}
+			if(0.0 < lengthAll && 0.0 < fValueAll){
+				fValueAll /= lengthAll;
+				color = HSLToRGB((1.0 - fValueAll)*240.0,1.0,0.5);
+			}
+			else{
+				color =vec4(0.0,0.0,1.0,1.0);
+			}
+		}
+	    vec3 lightDir = normalize(vec3(gl_LightSource[0].position));
+	    vec3 normal   = normalize(vecNormal);
+	    float NdotL   = max(dot(normal, lightDir), 0.0);
+	    gl_FragColor =  NdotL * color * gl_LightSource[0].diffuse +  color * gl_LightSource[0].ambient;
+	}
+)"
+
 class SceneViewer;
-class SceneCtrl :public QObject
-{
-	Q_OBJECT
-public:
-	SceneCtrl(SceneViewer* sv, QObject* parent=nullptr);
-	~SceneCtrl();
 
-	bool installSimRender(QVector<SensorPositon> sp,float max);
+	class SceneCtrl :public QObject
+	{
+		Q_OBJECT
+	public:
+		SceneCtrl(SceneViewer* sv, QObject* parent = nullptr);
+		~SceneCtrl();
 
-	bool uninstallSimRender();
+		bool installSimRender(QVector<SensorPositon> sp);
+		//0~1.0之间的值，提前算好再给进来
+		bool updateSimValues(QVector<float> values);
 
-private:
-	SceneViewer* _sceneViewer;
+		bool uninstallSimRender();
 
-	osg::ref_ptr<osg::Node> _model;
+	private:
+		SceneViewer* _sceneViewer;
 
-};
+		osg::ref_ptr<osg::Node> _model;
+		osg::ref_ptr<osg::Program> _simRender;
+
+	};
 
